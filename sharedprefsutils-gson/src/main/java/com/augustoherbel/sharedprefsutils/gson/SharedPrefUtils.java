e58e94eb2041c6ea81e0cbcb3881d60241c29f08
Augusto Herbel <augustoherbel.it@gmail.com>
2018-06-30 19:04:40 -0300

    Updates

---
+++ sharedprefsutils-gson/src/main/java/com/augustoherbel/sharedprefsutils/gson/SharedPrefUtils.java
@@ -0,0 +1,24 @@
+package com.augustoherbel.sharedprefsutils.gson;
+
+import com.augustoherbel.sharedprefsutils.BaseSharedPrefUtils;
+import com.google.gson.Gson;
+
+public class SharedPrefUtils extends BaseSharedPrefUtils {
+
+    public static void putObject(String keyName, Object keyValue) {
+        try {
+            putString(keyName, new Gson().toJson(keyValue));
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static <T> T getObject(String keyName, Class<T> keyValue) {
+        try {
+            return new Gson().fromJson(getString(keyName, ""), keyValue);
+        } catch (Exception | Error e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
