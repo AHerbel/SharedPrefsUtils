c6447b4432e6bb40da79d186634574f2beea800d
Augusto Herbel <augustoherbel.it@gmail.com>
2018-06-30 19:57:51 -0300

    erterte

--- sharedprefsutils-base/src/simple/java/com/augustoherbel/sharedprefsutils/simple/SharedPrefUtils.java
+++
@@ -1,7 +0,0 @@
-package com.augustoherbel.sharedprefsutils.simple;
-
-import com.augustoherbel.sharedprefsutils.BaseSharedPrefUtils;
-
-public class SharedPrefUtils extends BaseSharedPrefUtils {
-
-}
